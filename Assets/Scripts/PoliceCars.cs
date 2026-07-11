daa1709ad3eb9b25c45c77ebd4b55d2980aec368
Mahindra Vishal <dmahindravishal@gmail.com>
2026-07-11 17:03:27 +0530

    added police cars and helicopter sound

---
+++ Assets/Scripts/PoliceCars.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PoliceCars : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] GameObject Player;
+    [SerializeField] float distance;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position=new Vector3(0,0,Player.transform.position.z+distance);
+    }
+}
